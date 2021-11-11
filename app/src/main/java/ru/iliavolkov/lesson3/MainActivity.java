9f87bf2471c2751405ec56903bd6659959f0c29c
Илья <ilia.volkov2016@mail.ru>
2021-11-11 14:28:55 +0300

    btn1

--- app/src/main/java/ru/iliavolkov/lesson3/MainActivity.java
+++ app/src/main/java/ru/iliavolkov/lesson3/MainActivity.java
@@ -3,12 +3,29 @@ package ru.iliavolkov.lesson3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+
+    int counter1 = 0;
+    TextView textCounter1;
+    Button btn1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textCounter1 = findViewById(R.id.textView);
+        btn1 = findViewById(R.id.btn);
+
+        btn1.setOnClickListener(v -> {
+            counter1++;
+            textCounter1.setText(String.format(Locale.getDefault(), "%d", counter1));
+        });
+
     }
 }
